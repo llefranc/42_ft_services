3aa0fdd3c11096378595659bb9fb5a855e43765c
Lucas Lefrancq <lucaslefrancq@MacBook-Pro-de-Lucas.local>
2020-07-28 14:23:07 +0200

    phpmyadmin accessible mais probleme avec https

--- srcs/php backup/config.inc.php
+++ srcs/php backup/config.inc.php
@@ -15,7 +15,7 @@ declare(strict_types=1);
  * This is needed for cookie based authentication to encrypt password in
  * cookie. Needs to be 32 chars long.
  */
-$cfg['blowfish_secret'] = '$2a$07$EJooQ7FWQIpYWJAMqd0mq.eRnrTTAkqpIwEv1InrJ8q0KMfAK0WL'; /* YOU MUST FILL IN THIS FOR COOKIE AUTH! */
+$cfg['blowfish_secret'] = 'Sd[7AtCEkic{D{:isKqv5sEa9AsUVxD-'; /* YOU MUST FILL IN THIS FOR COOKIE AUTH! */
 
 /**
  * Servers configuration
