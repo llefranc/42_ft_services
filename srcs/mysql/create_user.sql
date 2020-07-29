07541d4b2bf3b3cf02520da79f7d08bd22d07588
Lucas Lefrancq <lucaslefrancq@MacBook-Pro-de-Lucas.local>
2020-07-29 14:44:55 +0200

    mysql connecte a phpmyadmin

--- srcs/mysql/create_user.sql
+++ srcs/mysql/create_user.sql
@@ -5,14 +5,18 @@
 
 -- id : user42
 -- password : user42
-CREATE USER 'user42'@'localhost' IDENTIFIED BY 'user42';
-GRANT ALL PRIVILEGES ON *.* TO 'user42'@'localhost' WITH GRANT OPTION;
+CREATE USER 'user42'@'%' IDENTIFIED BY 'user42';
+GRANT ALL PRIVILEGES ON *.* TO 'user42'@'%' WITH GRANT OPTION;
+
+CREATE USER 'root'@'%' IDENTIFIED BY 'root';
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;
 
 -- --------------------------------------------------------
 
 --
--- Database : `42_ft_server`
+-- Database : `42_ft_services`
 --
 CREATE DATABASE IF NOT EXISTS `42_ft_services`
   DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;
-USE 42_ft_server;
+USE 42_ft_services;
--- srcs/phpmyadmin/config.inc.php
+++ srcs/phpmyadmin/config.inc.php
@@ -29,7 +29,7 @@ $i++;
 /* Authentication type */
 $cfg['Servers'][$i]['auth_type'] = 'cookie';
 /* Server parameters */
-$cfg['Servers'][$i]['host'] = 'localhost';
+$cfg['Servers'][$i]['host'] = 'mysql-service';
 $cfg['Servers'][$i]['compress'] = false;
 $cfg['Servers'][$i]['AllowNoPassword'] = false;
 
