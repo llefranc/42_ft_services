88d69d631b0495a4abee433c276e36d4fd41749c
Lucas Lefrancq <lucaslefrancq@MacBook-Pro-de-Lucas.local>
2020-08-05 14:30:57 +0200

    wordpress connecte a mysql mais probleme sur l'affichage

--- srcs/mysql/create_user.sql
+++ srcs/mysql/create_user.sql
@@ -3,17 +3,17 @@
 
 -- --------------------------------------------------------
 
--- id : user42
--- password : user42
-CREATE USER 'user42'@'%' IDENTIFIED BY 'user42';
-GRANT ALL PRIVILEGES ON *.* TO 'user42'@'%' WITH GRANT OPTION;
-GRANT ALL PRIVILEGES ON *.* TO 'user42'@'localhost' WITH GRANT OPTION;
-
--- --------------------------------------------------------
-
 --
 -- Database : `42_ft_services`
 --
 CREATE DATABASE IF NOT EXISTS `42_ft_services`
   DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;
 USE 42_ft_services;
+
+-- --------------------------------------------------------
+
+-- id : root
+-- password : root
+CREATE USER 'root'@'%' IDENTIFIED BY 'root';
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;
