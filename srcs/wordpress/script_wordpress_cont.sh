ad244ed950f5e558082c493c684e498246d525e9
Lucas Lefrancq <lucaslefrancq@MacBook-Pro-de-Lucas.local>
2020-09-09 17:17:57 +0200

    phpmyadmin wordpress grafana et nginx en loadbalancer

--- srcs/wordpress/script_wordpress_cont.sh
+++ srcs/wordpress/script_wordpress_cont.sh
@@ -4,7 +4,7 @@ php-fpm7
 sleep 3
 
 # autoinstall of wordpress with wp-cli
-wp-cli --allow-root core install --path=/usr/share/webapp/wordpress --url=https://192.168.64.4/wordpress \
+wp-cli --allow-root core install --path=/usr/share/webapp/wordpress --url=http://192.168.64.7:5050/wordpress \
 --title="site_ft_services" --admin_user=user42 --admin_password=user42 --admin_email=xxx@xxx.fr
 # creating 2 wordpress users
 mysql -u user42 -puser42 -h mysql-service -D 42_ft_services -P 3306 < /home/script/create_user_wp.sql
